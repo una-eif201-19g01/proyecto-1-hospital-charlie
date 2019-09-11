3e51fed3f6050e48960658425387e9a19e27b7d3
Isabel-09 <53483708+Isabel-09@users.noreply.github.com>
2019-09-11 17:32:18 -0600

    menu

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -39,4 +39,62 @@ void Interfaz::menu(){
 	} while (opcion != 5);
 	return opcion;
 }
+//otro menu();
+do {
+	system("cls");
+	int respuesta = 0;
+	std::cout << "  Menú Hospital";
+	std::cout << " Hospital Esperanza";
+	std::cout << "  ====================================\n";
+	std::cout << "  1.Menu doctor ";
+	std::cout << "  2.Menu Paciente ";
+	std::cout << "  3.Imprimir Pabellones  ";
+	std::cout << "  4.Imprimir camas  ";
+	std::cout << "\n";
+	std::cout << "  3.  Exit\n";
+	std::cout << "  ====================================\n";
+	std::cout << "  Enter your selection: ";
+	std::cin >> respuesta;
+	std::cout << std::endl;
+	switch (respuesta) {
+	case 1:
+		system("cls");
+		int opcion;
+		std::cout << "  Menú Doctor";
+		std::cout << "  ====================================\n";
+		std::cout << "  1.Ingresar doctor ";
+		std::cout << "  2.Datos doctor    ";
+		std::cout << "  2.Eliminar Doctor";
 
+		break
+	case 2:
+		std::cout << "  Menú Paciente";
+		std::cout << "  ====================================\n";
+		std::cout << "  1.Ingresar Paciente ";
+		std::cout << "   2.Imprimir Paciente";
+		std::cout << "cambiar la cama";
+		std::cout << "  3.Eliminar Doctor";
+
+
+		break;
+
+	case 3:
+		cout << " Pabellones";
+		cout << "tostring";
+	case 4:
+		cou << "camas";
+		cout << "tostring";
+	case 5:
+		std::cout << "Hasta Luego.\n";
+		break;
+
+	default:
+		std::cout << respuesta << " es un número inválido\n";
+
+		std::cout << std::endl;
+
+	} while (respuesta != 5)
+		system("pause");
+}
+
+	}
